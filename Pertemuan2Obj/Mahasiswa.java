a7d6a6ec5894c468b427eb601256b2386b8d8673
daysri <53419167+daysri@users.noreply.github.com>
2026-02-24 14:01:35 +0700

    Percobaan modul

---
+++ Pertemuan2Obj/Mahasiswa.java
@@ -0,0 +1,13 @@
+package Pertemuan2Obj;
+
+public class Mahasiswa {
+    String Nama;
+    String Kelas;
+    String NIM;
+    double IPK;
+    public Mahasiswa (String N, String K){
+        this.Nama =N;
+        this.Kelas =K;
+    }
+
+}
---
+++ Pertemuan2Obj/Main.java
@@ -0,0 +1,11 @@
+package Pertemuan2Obj;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Mahasiswa Carlos =new Mahasiswa("carlos","1H");
+        Carlos.IPK = 4.0;
+        Mahasiswa Yasmin = new Mahasiswa("Yasmin", "1H");
+    }
+
+}
