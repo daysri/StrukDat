00364663f4859bba3e01e709a29484be36d24fef
daysri <53419167+daysri@users.noreply.github.com>
2026-03-03 13:42:38 +0700

    contoh

--- Pertemuan3AoObj/ArrayObjects.java
+++ Pertemuan3AoObj/ArrayObjects.java
@@ -2,7 +2,8 @@ package Pertemuan3AoObj;
 
 public class ArrayObjects {
     public static void main(String[] args) {
-        PersegiPanjang[] ppArray = new PersegiPanjang[3];       
+        PersegiPanjang[] ppArray = new PersegiPanjang[3]; 
+        //Integer[] contohArray = new Integer[9];     
         ppArray[0] = new PersegiPanjang(100, 20);
         ppArray[1] = new PersegiPanjang(120, 70);
         ppArray[2] = new PersegiPanjang(80, 40);
--- Pertemuan3AoObj/DaftarPersegiPanjang.java
+++ Pertemuan3AoObj/DaftarPersegiPanjang.java
@@ -4,6 +4,7 @@ public class DaftarPersegiPanjang {
     
     public PersegiPanjang PPterluas(PersegiPanjang[] arrayPersegiPanjang) {
         PersegiPanjang terluas = new PersegiPanjang();
+        PersegiPanjang tersempit =new PersegiPanjang(1,2);
         terluas = arrayPersegiPanjang[0];
         for (int i=1; i<arrayPersegiPanjang.length; i++) {
             if (arrayPersegiPanjang[i].luas()>terluas.luas()) {
