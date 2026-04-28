ca85bf0a0466b662a02b0519736bcd97fbb021d7
daysri <53419167+daysri@users.noreply.github.com>
2026-04-28 11:15:20 +0700

    Update ArrayObjects.java

--- Pertemuan3AoObj/ArrayObjects.java
+++ Pertemuan3AoObj/ArrayObjects.java
@@ -3,7 +3,7 @@ package Pertemuan3AoObj;
 public class ArrayObjects {
     public static void main(String[] args) {
         PersegiPanjang[] ppArray = new PersegiPanjang[3]; 
-        //Integer[] contohArray = new Integer[9];     
+        //Integer[][][] contohArray = new Integer[9][3][5];     
         ppArray[0] = new PersegiPanjang(100, 20);
         ppArray[1] = new PersegiPanjang(120, 70);
         ppArray[2] = new PersegiPanjang(80, 40);
